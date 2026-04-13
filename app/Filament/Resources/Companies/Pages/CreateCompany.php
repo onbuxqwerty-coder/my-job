396ef9f5b92c0b9a61c66469116835a2a11b0653
Volodymyr Sukhovoi <onbuxqwerty@gmail.com>
2026-04-13 12:47:34 +0300

    fix(admin): синхронізувати location з city_id при збереженні компанії

--- app/Filament/Resources/Companies/Pages/CreateCompany.php
+++ app/Filament/Resources/Companies/Pages/CreateCompany.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Companies\Pages;
 
 use App\Filament\Resources\Companies\CompanyResource;
+use App\Models\City;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateCompany extends CreateRecord
@@ -12,5 +13,14 @@ class CreateCompany extends CreateRecord
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
+    }
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        if (!empty($data['city_id'])) {
+            $data['location'] = City::find($data['city_id'])?->name ?? $data['location'] ?? '';
+        }
+
+        return $data;
     }
 }
--- app/Filament/Resources/Companies/Pages/EditCompany.php
+++ app/Filament/Resources/Companies/Pages/EditCompany.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Companies\Pages;
 
 use App\Filament\Resources\Companies\CompanyResource;
+use App\Models\City;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
@@ -13,6 +14,15 @@ class EditCompany extends EditRecord
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        if (!empty($data['city_id'])) {
+            $data['location'] = City::find($data['city_id'])?->name ?? $data['location'] ?? '';
+        }
+
+        return $data;
     }
 
     protected function getHeaderActions(): array
