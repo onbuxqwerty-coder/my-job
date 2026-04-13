b9a98b2486716de9212ffeec3c1cfa8edf80a81a
Volodymyr Sukhovoi <onbuxqwerty@gmail.com>
2026-04-13 12:43:33 +0300

    feat(admin): редірект на список компаній після збереження

--- app/Filament/Resources/Companies/Pages/EditCompany.php
+++ app/Filament/Resources/Companies/Pages/EditCompany.php
@@ -10,6 +10,11 @@ class EditCompany extends EditRecord
 {
     protected static string $resource = CompanyResource::class;
 
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
     protected function getHeaderActions(): array
     {
         return [
